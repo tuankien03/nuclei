32d8c6b12c14e2eeb25f5d35eeae8ce60c358b7d
tuankien03 <nguyentuankien03@gmail.com>
2025-05-11 14:17:26 +0700

    add workflow

--- web/app.js
+++ web/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
-app.get('/steal', (req, res) => {
-  console.log('[+] Cookie nhận được:', req.query.cookie);
+
+app.get('/test', (req, res) => {
+  console.log(`[+] nhận request:: http://localhost:${port}/test?pr=`,req.query.pr);
+//   console.log(`\nquery: `, req.query);
+//   console.log(`\nparams: `, req.params);
   res.send('OK');
 });
+
+
+app.post('/test', (req, res) => {
+  console.log(`[+] nhận request:: http://localhost:${port}/test?pr=`,req.query.pr);
+  console.log(`\nquery: `, req.query);
+  console.log(`\nparams: `, req.params);
+  res.send('OK');
+}
+);
 
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
